519bd1e19b10afa029754d4a73fb7d61f5ac2f88
Grant Heaslip <g@hslp.ca>
2019-08-20 23:12:20 -0400

    Miscellaneous Webpack configuration
    * Add new buildEnv config (to replace env config)
    
    * Add source maps in development
    
    * Update Procfiles
    
    * Add Webpack NPM scripts

--- src/lib/config.tsx
+++ src/lib/config.tsx
@@ -1,13 +1,9 @@
-const env =  process.env.APP_ENV || 'production';
-
 export const config = {
     assetsDir: 'assets',
+    buildEnv: process.env.BUILD_ENV,
     canonicalHost: process.env.CANONICAL_HOST,
     canonicalProtocol: process.env.CANONICAL_PROTOCOL,
-    env: env,
+    env: process.env.APP_ENV || 'production',
     port: process.env.PORT || 5000,
-    staticDir: env === 'development'
-        ? 'static'
-        : 'static-build',
     workers: process.env.WEB_CONCURRENCY || 1,
 };
--- webpack.config.js
+++ webpack.config.js
@@ -6,98 +6,107 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/server',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/',
-        filename: 'website-server.bundle.js'
-    },
-    node: {
-        '__dirname': false,
-    },
-    target: 'node',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx1']
-    },
-    plugins: [
-        new AssetsPlugin({
-            filename: 'temp/webpack-assets.json',
-        }),
-        new CleanWebpackPlugin(),
-        new CopyWebpackPlugin([
-            {
-                context: 'src',
-                from: 'static/**/*',
-                to: '.'
-            },
-        ]),
-        new webpack.DefinePlugin({
-            'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV) },
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'static/assets/css/website-[contenthash].css',
-        }),
-    ],
-    optimization: {
-        noEmitOnErrors: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx|js|jsx)$/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                    },
-                    {
-                        loader: 'linaria/loader',
-                    },
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(ico|png|txt)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            context: 'src',
-                            name: '[path][name]-[hash].[ext]',
-                            outputPath: 'static',
-                            publicPath: '/',
-                        },
-                    },
-                ]
-            },
-            {
-                test: /(manifest\.webmanifest|browserconfig\.xml)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            context: 'src',
-                            name: '[path][name]-[hash].[ext]',
-                            outputPath: 'static',
-                            publicPath: '/',
-                        }
-                    },
-                    {
-                        loader: 'app-manifest-loader',
-                    },
-                ],
-            },
+module.exports = function(env, argv) {
+    const isProductionBuild = (argv.mode === 'production');
+
+    return {
+        mode: 'development',
+        entry: './src/server',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            publicPath: '/',
+            filename: 'website-server.bundle.js'
+        },
+        'devtool': (
+            isProductionBuild
+                ? false
+                : 'cheap-module-eval-source-map'
+        ),
+        node: {
+            '__dirname': false,
+        },
+        target: 'node',
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx1']
+        },
+        plugins: [
+            new webpack.EnvironmentPlugin({
+                BUILD_ENV: argv.mode,
+            }),
+            new AssetsPlugin({
+                filename: 'temp/webpack-assets.json',
+            }),
+            new CleanWebpackPlugin(),
+            new CopyWebpackPlugin([
+                {
+                    context: 'src',
+                    from: 'static/**/*',
+                    to: '.'
+                },
+            ]),
+            new MiniCssExtractPlugin({
+                filename: 'static/assets/css/website-[contenthash].css',
+            }),
         ],
-    },
+        optimization: {
+            noEmitOnErrors: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(ts|tsx|js|jsx)$/,
+                    use: [
+                        {
+                            loader: 'babel-loader',
+                        },
+                        {
+                            loader: 'linaria/loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                        },
+                    ],
+                },
+                {
+                    test: /\.(ico|png|txt)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                context: 'src',
+                                name: '[path][name]-[hash].[ext]',
+                                outputPath: 'static',
+                                publicPath: '/',
+                            },
+                        },
+                    ]
+                },
+                {
+                    test: /(manifest\.webmanifest|browserconfig\.xml)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                context: 'src',
+                                name: '[path][name]-[hash].[ext]',
+                                outputPath: 'static',
+                                publicPath: '/',
+                            }
+                        },
+                        {
+                            loader: 'app-manifest-loader',
+                        },
+                    ],
+                },
+            ],
+        },
+    };
 };
